363c7e8855e89a92f1854e5aeb13100682fad775
Prem Prasad <premrinkinoniya@gmail.com>
2025-12-30 19:10:19 +0530

    updated commit

---
+++ controllers/payment.controller.js
@@ -0,0 +1,44 @@
+const razorpay=require('../config/razorpay');
+const crypto=require('crypto');
+
+exports.createOrder=async(req,res)=>{
+  try{
+    const{amount,courseId}=req.body;
+    const options={
+      amount:amount*100,
+      currency:"INR",
+      receipt:`course_${courseId}`
+    }
+    const order=await razorpay.orders.create(options);
+
+  
+  res.status(200).json({
+    success:true,
+    order
+  });
+  }catch(error){
+    res.status(500).json({error:error.message});
+  }
+}
+
+//verify Payment
+exports.verifyPayment=(req,res)=>{
+  const{razorpay_order_id,razorpay_payment_id,razorpay_signature}=req.body;
+  const body=razorpay_order_id+"|"+razorpay_payment_id;
+
+  const expectedSignature=crypto
+  .createHmac("sha256",process.env.RAZORPAY_KEY_SECRET)
+  .update(body.toString())
+  .digest("hex");
+  if(expectedSignature===razorpay_signature){
+    res.status(200).json({
+      success:true,
+      message:"Payment verified"
+    });
+  }else{
+    res.status(400).json({
+      success:false,
+      message:"Invalid signature"
+    })
+  }
+}
